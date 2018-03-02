088e9dab3fe33c270f48e0c0883efc391e4e487e
rRemix <568920427@qq.com>
2018-03-02 18:06:51 +0800

    v1.2.6.2

--- app/src/main/java/remix/myplayer/ui/fragment/LrcFragment.java
+++ app/src/main/java/remix/myplayer/ui/fragment/LrcFragment.java
@@ -2,8 +2,6 @@ package remix.myplayer.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +13,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.disposables.Disposable;
 import remix.myplayer.R;
-import remix.myplayer.adapter.LyricAdapter;
 import remix.myplayer.bean.mp3.Song;
 import remix.myplayer.interfaces.OnInflateFinishListener;
 import remix.myplayer.lyric.LrcView;
@@ -34,9 +31,9 @@ public class LrcFragment extends BaseFragment {
     private Song mInfo;
     @BindView(R.id.lrc_view)
     LrcView mLrcView;
-    @BindView(R.id.lrc_recyclerview)
-    RecyclerView mLyricRecyclerView;
-    private LyricAdapter mLyricAdapter;
+//    @BindView(R.id.lrc_recyclerview)
+//    RecyclerView mLyricRecyclerView;
+//    private LyricAdapter mLyricAdapter;
     //歌词
     private List<LrcRow> mLrcList;
 
@@ -62,10 +59,9 @@ public class LrcFragment extends BaseFragment {
             mOnFindListener.onViewInflateFinish(mLrcView);
         mInfo = getArguments().getParcelable("Song");
 
-        mLyricRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
-
-        mLyricAdapter = new LyricAdapter(mContext,R.layout.item_lyric,mLyricRecyclerView);
-        mLyricRecyclerView.setAdapter(mLyricAdapter);
+//        mLyricRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+//        mLyricAdapter = new LyricAdapter(mContext,R.layout.item_lyric,mLyricRecyclerView);
+//        mLyricRecyclerView.setAdapter(mLyricAdapter);
 
         return rootView;
     }
@@ -108,8 +104,8 @@ public class LrcFragment extends BaseFragment {
                         }
                         mLrcView.setLrcRows(mLrcList);
 
-                        mLyricAdapter.setData(mLrcList);
-                        mLyricAdapter.notifyDataSetChanged();
+//                        mLyricAdapter.setData(mLrcList);
+//                        mLyricAdapter.notifyDataSetChanged();
                     }
                 }, throwable -> {
                     if (id == mInfo.getId()) {
