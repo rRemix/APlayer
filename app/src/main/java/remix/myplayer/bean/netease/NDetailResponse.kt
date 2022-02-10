cf83499dd57c317f034add6e2cff9dd1b26537ea
qjzcj2008 <qjzcj2008@126.com>
2022-02-10 14:40:52 +0800

    Update NDetailResponse.kt

--- app/src/main/java/remix/myplayer/bean/netease/NDetailResponse.kt
+++ app/src/main/java/remix/myplayer/bean/netease/NDetailResponse.kt
@@ -4,9 +4,8 @@ data class NDetailResponse(val code: Int = 0,
                            val songs: List<SongDetailData>? = null) {
 
     data class SongDetailData(val id: Int,
-                              val album: SearchAlbumData? = null,
-                              val score: Int) {
+                              val album: SearchAlbumData? = null) {
 
             data class SearchAlbumData(val picUrl: String? = null)
-        }
     }
+}
