8de83b48bf122eed9dea26def2148fb8374ec6a7
rRemix <568920427@qq.com>
2026-01-21 20:12:10 +0800

    修复扫描后文件夹列表歌曲数量没有更新

--- app/src/main/java/remix/myplayer/data/model/audio/Folder.kt
+++ app/src/main/java/remix/myplayer/data/model/audio/Folder.kt
@@ -7,28 +7,18 @@ import java.io.Serial
  * Created by Remix on 2018/1/9.
  */
 @Serializable
-data class Folder(val name: String?,
-                  val count: Int,
-                  val path: String) : APlayerModel {
-
-  override fun equals(other: Any?): Boolean {
-    if (this === other) return true
-    if (javaClass != other?.javaClass) return false
-
-    other as Folder
-
-    return path == other.path
-  }
-
-  override fun hashCode(): Int {
-    return path.hashCode()
-  }
+data class Folder(
+  val name: String?,
+  val count: Int,
+  val path: String
+) : APlayerModel {
 
   override fun getKey(): String {
     return path
   }
 
   companion object {
+
     @Serial
     private const val serialVersionUID: Long = -7333769143033322264L
   }
