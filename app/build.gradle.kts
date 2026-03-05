4b09c3473f1b93fbf7a3147faed326432dda0a95
rRemix <568920427@qq.com>
2026-03-05 21:05:51 +0800

    v2.0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         minSdk = 21
         targetSdk = 35
 
-        versionCode = 20700
-        versionName = "2.0.7.0"
+        versionCode = 20800
+        versionName = "2.0.8.0"
 
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = true
